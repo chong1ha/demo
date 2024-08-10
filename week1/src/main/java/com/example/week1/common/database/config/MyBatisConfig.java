038f7bed747b3fac6866677045d691813f5f7210
chong1ha <zxcvbnm9931@gmail.com>
2024-08-11 00:17:37 +0900

    update

--- week1/src/main/java/com/example/week1/common/database/config/MyBatisConfig.java
+++ week1/src/main/java/com/example/week1/common/database/config/MyBatisConfig.java
@@ -38,11 +38,20 @@ public class MyBatisConfig {
     /**
      * SqlSessionFactory 생성
      *
+     * <p>
+     *     SqlSessionFactory는 SqlSession 객체를 생성하는 팩토리 클래스.
+     *     SqlSessionFactoryBean은 MyBatis의 SqlSessionFactory를 생성하는 Spring Bean.
+     *     이 Bean을 통해 Spring은 `SqlSessionFactory` 객체를 싱글톤으로 관리하며, 애플리케이션 컨텍스트에서 한 번만 생성.
+     *     SqlSession은 MyBatis에서 데이터베이스와의 상호작용을 담당하는 인터페이스 (SQL 쿼리를 실행하고 트랜잭션을 관리).
+     * </p>
+     *
      * @throws Exception SQL 세션 팩토리 생성 중 발생할 수 있는 예외
      * @return SqlSessionFactory
      */
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
+
+        // MyBatis의 세션 관리
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
@@ -52,13 +61,17 @@ public class MyBatisConfig {
     }
 
     /**
-     * SqlSessionTemplate 생성
+     * SqlSessionTemplate 생성 <br>
+     *
+     * thread-safe.
+     * SqlSessionTemplate은 MyBatis의 SqlSession을 대신하여 사용
      *
      * @param sqlSessionFactory SqlSessionFactory
      * @return SqlSessionTemplate
      */
     @Bean
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        // MyBatis의 세션을 스프링의 트랜잭션 관리와 통합
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
@@ -70,6 +83,7 @@ public class MyBatisConfig {
      */
     @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        // 데이터베이스 트랜잭션을 관리
         return new DataSourceTransactionManager(dataSource);
     }
 }
